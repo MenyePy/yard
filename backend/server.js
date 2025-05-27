25d8085b06f552e631a874b5616998f58f060084
Menye <menyeyt@gmail.com>
2025-05-27 21:55:53 +0200

    Vercel backend

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
   res.json("Backend, I guess. Menye's Market API");
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
