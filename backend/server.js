9d99f076857a974df7186c3189bdbfe8b361b2c2
Menye <menyeyt@gmail.com>
2025-05-28 22:19:32 +0200

    Update CORS since URL couldnt be changed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://menye-market.vercel.app', 'http://172.18.32.1:3000'],
+  origin: ['http://localhost:3000', 'https://afonnemarket.vercel.app', 'http://172.18.32.1:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin'],
